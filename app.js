7a967421a6a789d13ae449f3dbdb0758c71d1c1c
theubusu <80545678+theubusu@users.noreply.github.com>
2022-04-25 22:33:53 +0200

    what app

---
+++ app.js
@@ -0,0 +1,44 @@
+//Create an array where the message along with it's ID will be stored.
+let message = [];
+
+// This fuction will enables us to add the message to the DOM
+function addMessage(text){
+    //Object where message will be stored
+    const chat = {
+        text,
+        id: Date.now()
+    }
+
+    message.push(chat);
+    
+    //Render message to the screen
+    const list = document.querySelector('.messages');
+    list.insertAdjacentHTML('beforeend', 
+        `<p class="message-item" data-key="${chat.id}">
+            <span>${chat.text}</span>
+        </p>`
+
+    );
+
+}
+
+
+
+//Create event listener to detect when a message has been submitted
+const form = document.querySelector('.message-form');
+form.addEventListener('submit', event => {
+    event.preventDefault();
+
+    //input to save the message itself
+    const input = document.querySelector('.typedMessage');
+
+    //This helps us to detect empty messages and ignore them
+    const text = input.value.trim();
+
+    if(text !== ''){
+        addMessage(text);
+        input.value = '';
+        input.focus();
+        
+    }
+});
